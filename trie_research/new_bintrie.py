a829adb72ba35dd64e7dde6bc2464a8792986502
Vitalik Buterin <v@buterin.com>
2017-08-02 04:36:44 -0400

    Added branch fetching and verification

--- trie_research/new_bintrie.py
+++ trie_research/new_bintrie.py
@@ -149,19 +149,48 @@ def print_nodes(db, node, prefix=b''):
 
 def _get_branch(db, node, keypath):
     if not keypath:
-        return [node]
+        return [db.get(node)]
     L, R, nodetype = parse_node(db.get(node))
     if nodetype == KV_TYPE:
         path = encode_bin_path(L)
         if keypath[:len(L)] == L:
-            return [path] + _get_branch(db, R, keypath[len(L):])
+            return [b'\x01'+path] + _get_branch(db, R, keypath[len(L):])
         else:
-            return [path]
+            return [b'\x01'+path, db.get(R)]
     elif nodetype == BRANCH_TYPE:
         if keypath[:1] == b0:
-            return [R] + _get_branch(db, L, keypath[1:])
+            return [b'\x02'+R] + _get_branch(db, L, keypath[1:])
         else:
-            return [L] + _get_branch(db, R, keypath[1:])
+            return [b'\x03'+L] + _get_branch(db, R, keypath[1:])
+
+def _verify_branch(branch, root, keypath, value):
+    nodes = [branch[-1]]
+    _keypath = b''
+    for data in branch[-2::-1]:
+        marker, node = data[0], data[1:]
+        # it's a keypath
+        if marker == 1:
+            node = decode_bin_path(node)
+            _keypath = node + _keypath
+            nodes.insert(0, encode_kv_node(node, sha3(nodes[0])))
+        # it's a right-side branch
+        elif marker == 2:
+            _keypath = b0 + _keypath
+            nodes.insert(0, encode_branch_node(sha3(nodes[0]), node))
+        # it's a left-side branch
+        elif marker == 3:
+            _keypath = b1 + _keypath
+            nodes.insert(0, encode_branch_node(node, sha3(nodes[0])))
+        else:
+            raise Exception("Foo")
+        L, R, nodetype = parse_node(nodes[0])
+    if value:
+        assert _keypath == keypath
+    assert sha3(nodes[0]) == root
+    db = EphemDB()
+    db.kv = {sha3(node): node for node in nodes}
+    assert _get(db, root, keypath) == value
+    return True
 
 class Trie():
     def __init__(self, db, root):
@@ -174,7 +203,9 @@ class Trie():
         return _get(self.db, self.root, encode_bin(key))
 
     def get_branch(self, key):
-        return _get_branch(self.db, self.root, encode_bin(key))
+        o = _get_branch(self.db, self.root, encode_bin(key))
+        assert _verify_branch(o, self.root, encode_bin(key), self.get(key))
+        return o
 
     def update(self, key, value):
         assert len(key) == 20
--- trie_research/new_bintrie_tests.py
+++ trie_research/new_bintrie_tests.py
@@ -1,13 +1,14 @@
 from new_bintrie import Trie, EphemDB, encode_bin, encode_bin_path, decode_bin_path
 from ethereum.utils import sha3, encode_hex
 import random
+import rlp
 
 def shuffle_in_place(x):
     y = x[::]
     random.shuffle(y)
     return y
 
-kvpairs = [(sha3(str(i))[12:], str(i) * 5) for i in range(1000)]
+kvpairs = [(sha3(str(i))[12:], str(i) * 5) for i in range(2000)]
 
 
 for path in ([], [1,0,1], [0,0,1,0], [1,0,0,1,0], [1,0,0,1,0,0,1,0], [1,0] * 8):
@@ -15,19 +16,22 @@ for path in ([], [1,0,1], [0,0,1,0], [1,0,0,1,0], [1,0,0,1,0,0,1,0], [1,0] * 8):
 
 r1 = None
 
-for i in range(3):
+for _ in range(3):
     t = Trie(EphemDB(), b'')
-    for k, v in shuffle_in_place(kvpairs):
+    for i, (k, v) in enumerate(shuffle_in_place(kvpairs)):
         #print(t.to_dict())
         t.update(k, v)
         assert t.get(k) == v
-        if not random.randrange(100):
-            t.to_dict()
+        if not i % 50:
+            if not i % 250:
+                t.to_dict()
+            print("Length of branch at %d nodes: %d" % (i, len(rlp.encode(t.get_branch(k)))))
     assert r1 is None or t.root == r1
     r1 = t.root
     t.update(kvpairs[0][0], kvpairs[0][1])
     assert t.root == r1
     print(t.get_branch(kvpairs[0][0]))
+    print(t.get_branch(kvpairs[0][0][::-1]))
     print(encode_hex(t.root))
     for k, v in shuffle_in_place(kvpairs):
         t.update(k, b'')
