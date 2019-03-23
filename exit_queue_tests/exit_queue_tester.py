75fcd1da48f3e6ff95afb41731ff81ee03291115
Vitalik Buterin <v@buterin.com>
2019-03-23 07:36:47 -0500

    Made exit queue tester faster

--- exit_queue_tests/exit_queue_tester.py
+++ exit_queue_tests/exit_queue_tester.py
@@ -1,13 +1,17 @@
 from numpy.random import poisson
+import math
 
 # Target active staker size
 TARGET_AMOUNT_STAKING = 312500
 # Average time staking before withdrawal
-AVG_STAKING_TIME = 120
+AVG_STAKING_TIME = 500
 # How many withdrawals are permitted in
 # one day given a certain validator count?
-def withdrawals_per_day(validators):
-    return validators // 180
+def withdrawals_per_day(validators, total_eth_exiting):
+    # return (validators + total_eth_exiting) / 1.07 // 100
+    return validators // 100
+    # return validators * max(1, int(math.log2(total_eth_exiting))) / 13.5 // 100
+    # return int(1 + (total_eth_exiting * validators)**0.5) * 4.9 // 100
 
 # Get the size of the largest staker. This assumes a
 # Zipf's law distribution (ie. power law with power=1)
@@ -46,6 +50,8 @@ exiting = {}
 
 # The exit queue
 exit_queue = []
+# Total eth exiting
+total_eth_exiting = 0
 # How much of the first exiter's deposit we have processed
 processing_current = 0
 
@@ -59,6 +65,8 @@ for i, sz in enumerate(STAKER_SIZES):
 withdrawn = {}
 tot_delays = {}
 
+print("Total staking ETH:", sum(k * v for k,v in stakers.items()))
+
 for day in range(10000):
     # Deposit new stakers at the rate needed to maintain the equilibrium size
     for i, sz in enumerate(STAKER_SIZES):
@@ -67,33 +75,34 @@ for day in range(10000):
     
     # Each staker has a 1/AVG_STAKING_TIME probability of deciding to leave each day
     for k in stakers.keys():
-        for i in range(poisson(stakers[k] / AVG_STAKING_TIME)):
-            exit_queue.append((k, day))
-            stakers[k] -= 1
-            exiting[k] = exiting.get(k, 0) + 1
+        exit_count = min(poisson(stakers[k] / AVG_STAKING_TIME), stakers[k])
+        if exit_count > 0:
+            exit_queue.append((k, exit_count, day))
+            stakers[k] -= exit_count
+            exiting[k] = exiting.get(k, 0) + exit_count
+            total_eth_exiting += exit_count * k
     total_validators = sum(k * v for k,v in stakers.items()) + sum(k * v for k,v in exiting.items())
     
     # Process the queue
-    queue_to_empty_today = withdrawals_per_day(total_validators)
+    queue_to_empty_today = withdrawals_per_day(total_validators, total_eth_exiting)
     while queue_to_empty_today > 0 and len(exit_queue) > 0:
-        key, exit_day = exit_queue[0]
+        key, exit_count, exit_day = exit_queue[0]
         # Partially process the first exiter (exit next loop)
-        if key > queue_to_empty_today + processing_current:
+        if key * exit_count > queue_to_empty_today + processing_current:
             processing_current += queue_to_empty_today
             queue_to_empty_today = 0
         # Finish processing the first exiter (continue next loop)
         else:
             processing_current = 0
-            queue_to_empty_today -= key - processing_current
+            queue_to_empty_today -= key * exit_count - processing_current
             exit_queue.pop(0)
-            exiting[key] -= 1
-            withdrawn[key] = withdrawn.get(key, 0) + 1
-            tot_delays[key] = tot_delays.get(key, 0) + (day - exit_day)
-    if day % 100 == 99:
-        print("Report for day %d:" % (day+1))
-        print("Total validators:", total_validators)
-        print("Exit queue length:", len(exit_queue))
+            exiting[key] -= exit_count
+            total_eth_exiting -= exit_count * key
+            withdrawn[key] = withdrawn.get(key, 0) + exit_count
+            tot_delays[key] = tot_delays.get(key, 0) + (day - exit_day) * exit_count
+    if day % 1000 == 999:
+        print("Report for day %d: %d total validators %d ETH in exit queue" % ((day+1), total_validators, total_eth_exiting))
 
 print("Total delays in days")
 for key in STAKER_SIZES:
-    print("%d: % .3f (min %.3f)" % (key, (tot_delays.get(key, 0) / withdrawn.get(key, 0.0001)), key / withdrawals_per_day(TARGET_AMOUNT_STAKING)))
+    print("%d: % .3f (min %.3f)" % (key, (tot_delays.get(key, 0) / withdrawn.get(key, 0.0001)), key / withdrawals_per_day(TARGET_AMOUNT_STAKING, key)))
