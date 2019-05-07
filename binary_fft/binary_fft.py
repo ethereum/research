aa00c34691b6ada63f8b33261034d72f2cb0ac74
Vitalik Buterin <v@buterin.com>
2019-05-07 07:19:26 -0500

    Added comments

--- binary_fft/binary_fft.py
+++ binary_fft/binary_fft.py
@@ -157,44 +157,57 @@ def _simple_ft(field, vals):
     return [field.eval_poly_at(vals, i) for i in range(2**field.height)]
 
 # Returns `evens` and `odds` such that:
-# poly(x) = evens(x^2+kx) + x * odds(x^2+kx)
-# poly(x+k) = evens(x^2+kx) + (x+k) * odds(x^2+kx)
+# poly(x) = evens(x**2+kx) + x * odds(x**2+kx)
+# poly(x+k) = evens(x**2+kx) + (x+k) * odds(x**2+kx)
 # 
 # Note that this satisfies two other invariants:
 #
-# poly(x+k) - poly(x) = k * odds(x^2+kx)
-# poly(x)*(x+k) - poly(x+k)*x = k * evens(x^2+kx)
+# poly(x+k) - poly(x) = k * odds(x**2+kx)
+# poly(x)*(x+k) - poly(x+k)*x = k * evens(x**2+kx)
 
 def cast(field, poly, k):
     if len(poly) <= 2:
         return ([poly[0]], [poly[1] if len(poly) == 2 else 0])
+    # Largest mod_power=2**k such that mod_power >= len(poly)/2
     mod_power = 2
     while mod_power * 2 < len(poly):
         mod_power *= 2
     half_mod_power = mod_power // 2
     k_to_half_mod_power = field.exp(k, half_mod_power)
+    # Calculate low = poly % (x**2 - k*x)**half_mod_power
+    # and high = poly // (x**2 - k*x)**half_mod_power
+    # Note that (x**2 - k*x)**n = x**2n - k**n * x**n in binary fields
     low = poly + [0] * (mod_power * 2 - len(poly))
     high = low[len(low)-half_mod_power:]
     low = low[:len(low)-mod_power] + [low[i] ^ field.mul(low[i+half_mod_power], k_to_half_mod_power) for i in range(len(low)-mod_power, len(low)-half_mod_power)]
     high = low[len(low)-half_mod_power:] + high
     low = low[:len(low)-mod_power] + [low[i] ^ field.mul(low[i+half_mod_power], k_to_half_mod_power) for i in range(len(low)-mod_power, len(low)-half_mod_power)]
+    # Recursively compute two half-size sub-problems, low and high
     low_cast = cast(field, low, k) 
     high_cast = cast(field, high, k)
+    # Combine the results
     return (low_cast[0] + high_cast[0], low_cast[1] + high_cast[1])
 
-# Returns a polynomial p2 such that p2(x) = poly(x^2+kx)
+# Returns a polynomial p2 such that p2(x) = poly(x**2+kx)
 def compose(field, poly, k):
     if len(poly) == 1:
         return poly + [0]
+    # Largest mod_power=2**k such that mod_power >= len(poly)/2
     mod_power = 1
     while mod_power * 2 < len(poly):
         mod_power *= 2
     k_to_mod_power = field.exp(k, mod_power)
+    # Recursively compute two half-size sub-problems, the bottom and top half
+    # of the polynomial
     low = compose(field, poly[:mod_power], k) + [0] * mod_power * 3
     high = compose(field, poly[mod_power:], k) + [0] * mod_power * 3
+    # Combine them together, multiplying the top one by (x**2-k*x)**n
+    # Note that (x**2 - k*x)**n = x**2n - k**n * x**n in binary fields
     return [low[i] ^ field.mul(high[i-mod_power], k_to_mod_power) ^ high[i-2*mod_power] for i in range(mod_power*4)]
 
 # Equivalent to [field.eval_poly_at(poly, x) for x in domain]
+# Special thanks to www.math.clemson.edu/~sgao/papers/GM10.pdf for insights
+# though this algorithm is not exactly identical to any algorithm in the paper
 def fft(field, poly, domain):
     # Base case: constant polynomials
     if len(domain) == 1:
@@ -202,8 +215,8 @@ def fft(field, poly, domain):
     # Split the domain into two cosets A and B, where for x in A, x+offset is in B
     offset = domain[1]
     # Get evens, odds such that:
-    # poly(x) = evens(x^2+offset*x) + x * odds(x^2+offset*x)
-    # poly(x+k) = evens(x^2+offset*x) + (x+k) * odds(x^2+offset*x)
+    # poly(x) = evens(x**2+offset*x) + x * odds(x**2+offset*x)
+    # poly(x+k) = evens(x**2+offset*x) + (x+k) * odds(x**2+offset*x)
     evens, odds = cast(field, poly, offset)
     # The smaller domain D = [x**2 - offset*x for x in A] = [x**2 - offset*x for x in B]
     casted_domain = [field.mul(x, offset ^ x) for x in domain][::2]
@@ -224,8 +237,8 @@ def invfft(field, vals, domain):
     # Split the domain into two cosets A and B, where for x in A, x+offset is in B
     offset = domain[1]
     # Compute the evaluations of the evens and odds polynomials using the invariants:
-    # poly(x+k) - poly(x) = k * odds(x^2+kx)
-    # poly(x)*(x+k) - poly(x+k)*x = k * evens(x^2+kx)
+    # poly(x+k) - poly(x) = k * odds(x**2+kx)
+    # poly(x)*(x+k) - poly(x+k)*x = k * evens(x**2+kx)
     L, R = vals[::2], vals[1::2]
     even_points = [field.div(field.mul(l, d ^ offset) ^ field.mul(r, d), offset) for d, l, r in zip(domain[::2], L, R)]
     odd_points = [field.div(l ^ r, offset) for d, l, r in zip(domain[::2], L, R)]
@@ -235,7 +248,7 @@ def invfft(field, vals, domain):
     # the polynomials evens and odds
     evens = invfft(field, even_points, casted_domain)
     odds = invfft(field, odd_points, casted_domain)
-    # Given evens and odds where poly(x) = evens(x^2+offset*x) + x * odds(x^2+offset*x),
+    # Given evens and odds where poly(x) = evens(x**2+offset*x) + x * odds(x**2+offset*x),
     # recover poly
     composed_evens = compose(field, evens, offset) + [0]
     composed_odds = compose(field, odds, offset) + [0]
--- binary_fft/test_binary_fft.py
+++ binary_fft/test_binary_fft.py
@@ -1,4 +1,4 @@
-import poly_utils as binary_fft
+import binary_fft
 
 bigf = binary_fft.BinaryField(1033)
 poly = [x**9 % 1024 for x in range(1024)]
