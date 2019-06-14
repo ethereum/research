eaa3efe0d2ffa0c6297682d87eab011f70fff524
Vitalik Buterin <v@buterin.com>
2019-06-14 11:13:14 -0400

    Made simple types simpler

--- newssz/foo.py
+++ newssz/foo.py
@@ -18,20 +18,20 @@ class AbstractListMeta(type):
             setattr(out, 'length', attrs['length'])
         return out
 
-    child = lambda self: AbstractList
-
     def __getitem__(self, params):
         if not isinstance(params, tuple) or len(params) != 2:
             raise Exception("List must be instantiated with two args: elem type and length")
-        o = self.__class__(self.__name__, (self.child(),), {'elem_type': params[0], 'length': params[1]})
+        o = self.__class__(self.__name__, (self,), {'elem_type': params[0], 'length': params[1]})
         o._name = 'AbstractList'
         return o
 
     def __instancecheck__(self, obj):
-        if not issubclass(obj.__class__, self.child()):
+        if obj.__class__.__name__ != self.__name__:
             return False
-        if hasattr(self, 'elem_type'):
-            return obj.__class__.elem_type == self.elem_type and obj.__class__.length == self.length
+        if hasattr(self, 'elem_type') and obj.__class__.elem_type != self.elem_type:
+            return False
+        if hasattr(self, 'length') and obj.__class__.length != self.length:
+            return False
         return True
 
 class ValueCheckError(Exception):
@@ -39,7 +39,7 @@ class ValueCheckError(Exception):
 
 class AbstractList(metaclass=AbstractListMeta):
     def __init__(self, *args):
-        items = args[0] if len(args) else self.default()
+        items = self.extract_args(args)
             
         if not self.value_check(items):
             raise ValueCheckError("Bad input for class {}: {}".format(self.__class__, items))
@@ -51,11 +51,17 @@ class AbstractList(metaclass=AbstractListMeta):
                 return False
         return True
 
+    def extract_args(self, args):
+        return list(args) if len(args) > 0 else self.default()
+
     def default(self):
         raise Exception("Not implemented")
 
     def __getitem__(self, i):
         return self.items[i]
+
+    def __setitem__(self, k, v):
+        self.items[k] = v
 
     def __len__(self):
         return len(self.items)
@@ -63,20 +69,20 @@ class AbstractList(metaclass=AbstractListMeta):
     def __repr__(self):
         return repr(self.items)
 
-class ListMeta(AbstractListMeta):
-    child = lambda self: List
+    def __iter__(self):
+        return iter(self.items)
 
-class List(AbstractList, metaclass=ListMeta):
+    def __eq__(self, other):
+        return self.items == other.items
+
+class List(AbstractList, metaclass=AbstractListMeta):
     def value_check(self, value):
         return len(value) <= self.__class__.length and super().value_check(value)
 
     def default(self):
         return []
 
-class VectorMeta(AbstractListMeta):
-    child = lambda self: Vector
-
-class Vector(AbstractList, metaclass=VectorMeta):
+class Vector(AbstractList, metaclass=AbstractListMeta):
     def value_check(self, value):
         return len(value) == self.__class__.length and super().value_check(value)
 
@@ -84,35 +90,31 @@ class Vector(AbstractList, metaclass=VectorMeta):
         return [get_default_value(self.__class__.elem_type) for _ in range(self.__class__.length)]
 
 class BytesMeta(AbstractListMeta):
-    child = lambda self: Bytes
-
     def __getitem__(self, params):
         if not isinstance(params, int):
             raise Exception("Bytes must be instantiated with one arg: length")
-        o = self.__class__(self.__name__, (self.child(),), {'length': params})
+        o = self.__class__(self.__name__, (self,), {'length': params})
         o._name = 'Bytes'
         return o
+
+def single_item_extractor(cls, args):
+    assert len(args) < 2
+    return args[0] if len(args) > 0 else cls.default()
 
 class Bytes(AbstractList, metaclass=BytesMeta):
     def value_check(self, value):
         return len(value) <= self.__class__.length and isinstance(value, bytes)
 
+    extract_args = single_item_extractor
+
     def default(self):
         return b''
 
-class BytesNMeta(AbstractListMeta):
-    child = lambda self: BytesN
-
-    def __getitem__(self, params):
-        if not isinstance(params, int):
-            raise Exception("Bytes must be instantiated with one arg: length")
-        o = self.__class__(self.__name__, (self.child(),), {'length': params})
-        o._name = 'Bytes'
-        return o
-
-class BytesN(AbstractList, metaclass=BytesNMeta):
+class BytesN(AbstractList, metaclass=BytesMeta):
     def value_check(self, value):
         return len(value) == self.__class__.length and isinstance(value, bytes)
+
+    extract_args = single_item_extractor
 
     def default(self):
         return b'\x00' * self.__class__.length
--- newssz/test_foo.py
+++ newssz/test_foo.py
@@ -1,18 +1,27 @@
 from foo import List, Vector, Bytes, BytesN, ValueCheckError
 
 def assert_pass(typ, value):
-    x = typ(value)
+    x = typ(*value) if issubclass(typ, List) or issubclass(typ, Vector) else typ(value)
     for i in range(len(value)):
         assert x[i] == value[i]
 
 def assert_invalid(typ, value):
     try:
-        x = typ(value)
+        x = typ(*value) if issubclass(typ, List) or issubclass(typ, Vector) else typ(value)
         passed = True
     except ValueCheckError:
         passed = False
     assert not passed
 
+assert isinstance(List[int, 3](10, 20, 30), List[int, 3])
+assert not isinstance(List[int, 3](10, 20, 30), List[int, 5])
+assert not isinstance(List[int, 3](10, 20, 30), List[bool, 3])
+assert isinstance(List[int, 3](10, 20, 30), List)
+assert isinstance(Vector[int, 3](10, 20, 30), Vector[int, 3])
+assert not isinstance(Vector[int, 3](10, 20, 30), List[int, 3])
+assert isinstance(Bytes[3](b"cow"), Bytes[3])
+assert not isinstance(Bytes[3](b"cow"), Bytes[5])
+assert isinstance(Bytes[3](b"cow"), Bytes)
 assert_pass(List[int, 3], [])
 assert_pass(List[int, 3], [10])
 assert_pass(List[int, 3], [10, 20])
@@ -20,13 +29,13 @@ assert_pass(List[int, 3], [10, 20, 30])
 assert_invalid(List[int, 3], [10, 20, 30, 40])
 assert_invalid(List[int, 3], ["dog", 20, 30])
 assert_pass(Vector[int, 3], [10, 20, 30])
-assert_invalid(Vector[int, 3], [])
+assert_invalid(Vector[int, 3], [10])
 assert_invalid(Vector[int, 3], [10, 20, 30, 40])
 assert_invalid(Vector[int, 3], [10, "cow", 30])
-assert_pass(Vector[Vector[int, 2], 2], [Vector[int, 2]([10, 20]), Vector[int, 2]([30, 40])])
-assert_invalid(Vector[Vector[int, 2], 2], [Vector[int, 2]([10, 20]), Vector[int, 3]([30, 40, 50])])
-assert_invalid(Vector[Vector[int, 2], 2], [Vector[int, 2]([10, 20]), Vector[int, 2]([30, 40]), Vector[int, 2]([50, 60])])
-assert_invalid(Vector[Vector[int, 2], 2], [Vector[int, 2]([10, 20]), List[int, 2]([30, 40])])
+assert_pass(Vector[Vector[int, 2], 2], [Vector[int, 2](10, 20), Vector[int, 2](30, 40)])
+assert_invalid(Vector[Vector[int, 2], 2], [Vector[int, 2](10, 20), Vector[int, 3](30, 40, 50)])
+assert_invalid(Vector[Vector[int, 2], 2], [Vector[int, 2](10, 20), Vector[int, 2](30, 40), Vector[int, 2](50, 60)])
+assert_invalid(Vector[Vector[int, 2], 2], [Vector[int, 2](10, 20), List[int, 2](30, 40)])
 assert_pass(Bytes[3], b'ow')
 assert_pass(Bytes[3], b'cow')
 assert_invalid(Bytes[3], b'crow')
@@ -35,3 +44,7 @@ assert_invalid(BytesN[3], b'ow')
 assert_pass(BytesN[3], b'cow')
 assert_invalid(BytesN[3], b'crow')
 assert_invalid(BytesN[3], [10, 20, 30])
+x = List[int, 3](10, 20, 30)
+x[2] = 100
+assert x == List[int, 3](10, 20, 100)
+print("Tests passed")
