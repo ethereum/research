da7bbc42b59a046abc3f3c03f1ed4700bc4add22
Dankrad Feist <mail@dankradfeist.de>
2020-12-26 11:31:33 +0000

    Methods for constructing zero polynomials and reconstructing polynomials from partial evaluations on a subgroup

---
+++ polynomial_reconstruction/poly_gcd.py
@@ -0,0 +1,146 @@
+from poly_utils import PrimeField
+from fft import fft
+from math import prod
+
+def next_power_of_two(x):
+    return 2**((x - 1).bit_length())
+
+class PrimeFieldExtended(PrimeField):
+
+    def __init__(self, modulus, primitive_root):
+        assert pow(2, modulus, modulus) == 2
+        self.modulus = modulus
+        assert pow(primitive_root, modulus - 1, modulus) == 1
+        assert pow(primitive_root, (modulus - 1) // 2, modulus) != 1
+        self.primitive_root = primitive_root
+
+    def mul_many_polys(self, ps):
+        n = next_power_of_two(sum(self.degree(p) for p in ps) + 1)
+        if (self.modulus - 1) % n == 0:
+            root_of_unity = pow(self.primitive_root, (self.modulus - 1) // n, self.modulus)
+            ps_fft = [fft(p, self.modulus, root_of_unity) for p in ps]
+            r = [prod(l) for l in zip(*ps_fft)]
+            return self.truncate_poly(fft(r, self.modulus, root_of_unity, inv=True))
+        else:
+            return reduce(lambda a, b: super().mul_polys(a, b), ps)
+
+    def mul_polys(self, a, b):
+        if self.degree(a) is None or self.degree(b) is None or self.degree(a) < 64 or self.degree(b) < 64:
+            return super().mul_polys(a, b)
+        else:
+            n = next_power_of_two(self.degree(a) + self.degree(b) + 1)
+            if (self.modulus - 1) % n == 0:
+                root_of_unity = pow(self.primitive_root, (self.modulus - 1) // n, self.modulus)
+                assert pow(root_of_unity, n, self.modulus) == 1
+                assert pow(root_of_unity, n // 2, self.modulus) != 1
+                a_fft = fft(self.truncate_poly(a), self.modulus, root_of_unity)
+                b_fft = fft(self.truncate_poly(b), self.modulus, root_of_unity)
+                r = [x * y for x, y in zip(a_fft, b_fft)]
+                return self.truncate_poly(fft(r, self.modulus, root_of_unity, inv=True))
+            else:
+                return super().mul_polys(a, b)
+            
+    
+    def degree(self, p):
+        for i in range(len(p) - 1, -1, -1):
+            if p[i] % self.modulus != 0:
+                return i
+
+    def leading_coefficient(self, p):
+        if self.degree(p) is None:
+            return 1
+        return p[self.degree(p)]
+    
+    def truncate_poly(self, p):
+        if self.degree(p) is not None:
+            return p[:self.degree(p) + 1]
+        return []
+
+    def poly_quotient_remainder(self, p, q):
+        if self.degree(p) is None or self.degree(p) < self.degree(q):
+            return ([], p)
+        else:
+            leading_p = self.leading_coefficient(p)
+            leading_q = self.leading_coefficient(q)
+            factor = self.div(leading_p, leading_q)
+            part_quotient = [0] * (self.degree(p) - self.degree(q)) + [self.div(leading_p, leading_q)]
+            part_quotient_times_q = [0] * (self.degree(p) - self.degree(q)) + [self.mul(factor, x) for x in q]
+            part_remainder = self.sub_polys(p, part_quotient_times_q)
+            part2_quotient, remainder = self.poly_quotient_remainder(part_remainder, q)
+            return self.truncate_poly(self.add_polys(part_quotient, part2_quotient)), self.truncate_poly(remainder)
+    
+    
+    def multiply_poly_mat_vec(self, A, v):
+        assert all(len(row) == len(v) for row in A)
+        result = []
+        for row in A:
+            r = []
+            for a, b in zip(row, v):
+                r = self.add_polys(r, self.mul_polys(a, b))
+            result.append(r)
+        return result
+    
+    def multiply_poly_matrices(self, A, B):
+        return list(zip(*[self.multiply_poly_mat_vec(A, x) for x in zip(*B)]))
+
+    def poly_quo(self, p, k):
+        if k >= 0:
+            return p[k:]
+        else:
+            return p
+    
+    def M_hgcd(self, a, b):
+        # https://www.csd.uwo.ca/~mmorenom/CS424/Lectures/FastDivisionAndGcd.html/node6.html
+        d = self.degree(a)
+        if d is None:
+            return [[[1],[0]],[[0],[1]]]
+        m = (d + 1) // 2
+        if self.degree(b) is None or self.degree(b) == 0 or self.degree(b) < m:
+            return [[[1],[0]],[[0],[1]]]
+        a_quo = self.poly_quo(a, m)
+        b_quo = self.poly_quo(b, m)
+        M1 = self.M_hgcd(a_quo, b_quo)
+        t, s = self.multiply_poly_mat_vec(M1, (a, b))
+        if self.degree(s) is None:
+            return M1
+        q, r = self.poly_quotient_remainder(t, s)
+        if self.degree(r) is None:
+            M2 = [[[0],[1]],[[1],[-x for x in q]]]
+            return self.multiply_poly_matrices(M2, M1)
+        v = self.inv(self.leading_coefficient(r))
+        r_ = self.mul_polys(r, [v])
+        M2 = [[[0],[1]],[[v],self.mul_polys([-v], q)]]
+        l = 2 * m - self.degree(s)
+        s_quo = self.poly_quo(s, l)
+        r_quo = self.poly_quo(r_, l)
+        assert self.degree(s_quo) is None or self.degree(r_quo) is None or self.degree(s_quo) >= self.degree(s_quo)
+        M3 = self.M_hgcd(s_quo, r_quo)
+        return self.multiply_poly_matrices(M3, self.multiply_poly_matrices(M2, M1))
+    
+    def M_gcd(self, a, b):
+        M1 = self.M_hgcd(a, b)
+        t, s = self.multiply_poly_mat_vec(M1, [a, b])
+        if self.degree(s) is None:
+            return M1
+        q, r = self.poly_quotient_remainder(t, s)
+        if self.degree(r) is None:
+            M2 = [[[0],[1]],[[1],[-x for x in q]]]
+            return self.multiply_poly_matrices(M2, M1)
+        v = self.inv(self.leading_coefficient(r))
+        r_ = self.mul_polys(r, [v])
+        M2 = [[[0],[1]],[[v],self.mul_polys([-v], q)]]
+        M3 = self.M_hgcd(s, r_)
+        return self.multiply_poly_matrices(M3, self.multiply_poly_matrices(M2, M1))
+        
+    def fast_extended_euclidean_algorithm(self, a, b):
+        if self.degree(a) == self.degree(b):
+            return self.fast_extended_euclidean_algorithm(b, self.poly_quotient_remainder(a, b)[1])
+        elif self.degree(b) is None or self.degree(a) > self.degree(b):
+            M = self.M_gcd(a, b)
+            g = self.add_polys(self.mul_polys(M[0][0], a), self.mul_polys(M[0][1], b))
+            v = self.inv(self.leading_coefficient(g))
+            return self.mul_polys(self.truncate_poly(g), [v]), \
+                   self.mul_polys(self.truncate_poly(M[0][1]), [v]), \
+                   self.mul_polys(self.truncate_poly(M[0][0]), [v])
+        else:
+            return self.fast_extended_euclidean_algorithm(b, a)
---
+++ polynomial_reconstruction/polynomial_reconstruction.py
@@ -0,0 +1,108 @@
+from poly_gcd import PrimeFieldExtended
+from fft import fft, shift_poly
+from random import randint
+from time import time
+import py_ecc.optimized_bls12_381 as b
+
+MODULUS = b.curve_order
+
+PRIMITIVE_ROOT_OF_UNITY = 5
+
+primefield = PrimeFieldExtended(MODULUS, PRIMITIVE_ROOT_OF_UNITY)
+
+assert pow(PRIMITIVE_ROOT_OF_UNITY, MODULUS - 1, MODULUS) == 1
+assert pow(PRIMITIVE_ROOT_OF_UNITY, (MODULUS - 1) // 2, MODULUS) != 1
+
+n = 2**15
+ROOT_OF_UNITY = pow(PRIMITIVE_ROOT_OF_UNITY, (MODULUS - 1)//n, MODULUS)
+DOMAIN = [pow(ROOT_OF_UNITY, i, MODULUS) for i in range(n)]
+
+def zero_polynomial_via_gcd(root_of_unity, zero_vector):
+    e1 = [0 if x == 0 or i % 2 == 0 else 1 for i, x in enumerate(zero_vector)]
+    e2 = [0 if x == 0 or i % 2 == 1 else 1 for i, x in enumerate(zero_vector)]
+    p1 = fft(e1, primefield.modulus, root_of_unity, inv=True)
+    p2 = fft(e2, primefield.modulus, root_of_unity, inv=True)
+    zero_poly = primefield.fast_extended_euclidean_algorithm(p1, p2)[0]
+    assert primefield.degree(zero_poly) == len(list(filter(lambda x: x == 0, zero_vector)))
+    r = fft(zero_poly, primefield.modulus, root_of_unity)
+    assert all(a == 0 and b == 0 or a != 0 and b != 0 for a, b in zip(zero_vector, r))
+    return r, zero_poly
+
+
+def zero_polynomial_via_multiplication(root_of_unity, zero_vector):
+    reduction_factor = 4
+    ps = [[1]]
+    for i, x in enumerate(zero_vector):
+        if x == 0:
+            if primefield.degree(ps[-1]) < 63:
+                ps[-1] = primefield.mul_polys(ps[-1], [-pow(root_of_unity, i, primefield.modulus), 1])
+            else:
+                ps.append([-pow(root_of_unity, i, primefield.modulus), 1])
+    while len(ps) > 1:
+        if len(ps) < 2 * reduction_factor and len(ps) > reduction_factor:
+            psnew = [primefield.mul_many_polys(ps[:len(ps) // 2]), primefield.mul_many_polys(ps[len(ps) // 2:])]
+        else:
+            psnew = [primefield.mul_many_polys(l) for l in zip(*[ps[i::reduction_factor] for i in range(reduction_factor)])]
+            if len(ps) % reduction_factor != 0:
+                psnew.append(primefield.mul_many_polys(ps[- (len(ps) % reduction_factor):]))
+        ps = psnew
+    zero_poly = ps[0]
+    assert primefield.degree(zero_poly) == len(list(filter(lambda x: x == 0, zero_vector)))
+    r = fft(zero_poly, primefield.modulus, root_of_unity)
+    assert all(a == 0 and b == 0 or a != 0 and b != 0 for a, b in zip(zero_vector, r))
+    return r, zero_poly
+
+def reconstruct_polynomial_from_samples(root_of_unity, samples, zero_polynomial_function):
+    zero_vector = [0 if x is None else 1 for x in samples]
+
+    time_a = time()
+    zero_eval, zero_poly = zero_polynomial_function(root_of_unity, zero_vector)
+    time_b = time()
+
+    poly_evaluations_with_zero = [(0 if x is None else x) * y for x, y in zip(samples, zero_eval)]
+    poly_with_zero = fft(poly_evaluations_with_zero, MODULUS, root_of_unity, inv=True)
+
+    shift_factor = PRIMITIVE_ROOT_OF_UNITY
+    shift_inv = primefield.inv(PRIMITIVE_ROOT_OF_UNITY)
+    
+    shifted_poly_with_zero = shift_poly(poly_with_zero, MODULUS, PRIMITIVE_ROOT_OF_UNITY)
+    shifted_zero_poly = shift_poly(zero_poly, MODULUS, PRIMITIVE_ROOT_OF_UNITY)
+    
+    eval_shifted_poly_with_zero = fft(shifted_poly_with_zero, MODULUS, ROOT_OF_UNITY)
+    eval_shifted_zero_poly = fft(shifted_zero_poly, MODULUS, ROOT_OF_UNITY)
+    
+    eval_shifted_reconstructed_poly = [primefield.div(a, b) for a, b in 
+                                       zip(eval_shifted_poly_with_zero, eval_shifted_zero_poly)]
+    
+    shifted_reconstructed_poly = fft(eval_shifted_reconstructed_poly, MODULUS, ROOT_OF_UNITY, inv=True)
+    
+    reconstructed_poly = shift_poly(shifted_reconstructed_poly, MODULUS, shift_inv)
+
+    reconstructed_data = fft(reconstructed_poly, MODULUS, ROOT_OF_UNITY)
+    
+    assert all(x is None or x == y for x, y in zip(samples, reconstructed_data))
+    
+    return reconstructed_data, time_b - time_a
+
+
+if __name__ == "__main__":
+    poly = [i % 10 for i in range(n // 2)]
+    data = fft(poly, MODULUS, ROOT_OF_UNITY)
+    samples = data[:]
+    for i in range(n//2):
+        j = randint(0, n-1)
+        while samples[j] == None:
+            j = randint(0, n-1)
+        samples[j] = None
+
+    print("Computed data samples")
+    time_a = time()
+    reconstructed_data, zero_time_a = reconstruct_polynomial_from_samples(ROOT_OF_UNITY, samples, zero_polynomial_via_multiplication)
+    time_b = time()
+    print("Reconstructed data using zero_polynomial_via_multiplication in {0:.2f} s (of which constructing zero poly: {1:.2f} s)".format(time_b - time_a, zero_time_a))
+    assert reconstructed_data == data
+
+    reconstructed_data2, zero_time_b = reconstruct_polynomial_from_samples(ROOT_OF_UNITY, samples, zero_polynomial_via_gcd)
+    time_c = time()
+    print("Reconstructed data using zero_polynomial_via_gcd in {0:.2f} s (of which constructing zero poly: {1:.2f} s)".format(time_c - time_b, zero_time_b))
+    assert reconstructed_data == data
