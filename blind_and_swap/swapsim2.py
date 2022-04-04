8bf89edc88c5abc6f0d68ea1f99b2d3d31553383
vub <vub@pop-os.localdomain>
2022-04-04 02:05:19 -0400

    Added updated swap sim

--- blind_and_swap/swapsim2.py
+++ blind_and_swap/swapsim2.py
@@ -1,50 +1,97 @@
 import copy, sys, random
 
+LOG_LEVEL = [None]
+FUNCTION = (lambda: 0).__class__
+
+DEFAULTS = {
+    'width': 256,
+    'rounds': 512,
+    'swaps_per_round': 15,
+    'log_level': 1
+}
+
 def merge_probs(*probs):
+    L = len(probs)
+    #if isinstance(probs[0], list):
+    #    return [sum(prob[i]) / len(probs)
     o = {}
     for prob in probs: 
         for k,v in prob.items():
-            o[k] = o.get(k, 0) + v / len(probs)
+            o[k] = o.get(k, 0) + v / L
     return o
 
-def run(width, rounds, swaps_per_round, extract_positions):
+def simplify_dict(obj):
+    o = {}
+    for x in sorted(obj.keys(), key=lambda z: obj[z], reverse=True)[:4]:
+        o[x] = round(obj[x], 3)
+    if sum(o.values()) < 1:
+        o['other'] = round(obj[x], 3)
+    return o
+
+def log(contents, level):
+    if level <= LOG_LEVEL[0]:
+        if isinstance(contents, FUNCTION):
+            contents = contents()
+        print(contents)
+
+def run(width, rounds, swaps_per_round):
+    extract_positions = [random.randrange(width) for _ in range(rounds + swaps_per_round)]
+    extract_offsets = [random.randrange(width//2) * 2 + 1 for _ in range(rounds + swaps_per_round)]
     array = [{i: 1} for i in range(width)]
     o = []
+    if bin(width).count('1') != 1:
+        raise Exception("Width must be a power of 2")
+    if bin(swaps_per_round + 1).count('1') != 1:
+        raise Exception("swaps_per_round must be a power of 2 minus 1")
+    log_swaps = len(bin(swaps_per_round + 1)) - 3
     for r in range(rounds):
-        offsets = [(r*2+1)*i for i in range(swaps_per_round)]
-        swap_indices = [(extract_positions[r-1] + offset) % width for offset in offsets]
-        merged_probs = merge_probs(*[array[index] for index in swap_indices])
-        for index in swap_indices:
-            array[index] = copy.copy(merged_probs)
+        log(" ------ ", 1)
+        log("Round {}".format(r), 1)
+        for depth in range(log_swaps):
+            pivot = extract_positions[r - depth - 1]
+            offset = extract_offsets[r - depth - 1]
+            log("Depth {}: pivot {} offset {}".format(depth, pivot, offset), 2)
+            for i in range(2**depth):
+                L = (pivot + offset * i) % width
+                R = (pivot + offset * (i + 2**depth)) % width
+                log("Swapping {} and {}".format(L, R), 2)
+                new_prob = merge_probs(array[L], array[R])
+                array[L] = new_prob
+                array[R] = new_prob
         extraction_index = extract_positions[r]
         o.append(array[extraction_index])
         array[extraction_index] = {width+r: 1}
+        log("New index: {}, Pivot: {}, offset: {}".format(width+r, extraction_index, extract_offsets[r]), 2)
+        log(lambda: [simplify_dict(x) for x in array], 3)
     return o
 
 def test(width, rounds, swaps_per_round):
-    extract_positions = [random.randrange(width) for _ in range(rounds + swaps_per_round)]
     thresholds = []
-    outputs = run(width, rounds, swaps_per_round, extract_positions)
+    outputs = run(width, rounds, swaps_per_round)
     for store in outputs:
         top_freqs = sorted(store.values(), reverse=True)
-        # print(top_freqs)
         await_count = sum(top_freqs) * 0.2
         for i in range(len(top_freqs)):
             await_count -= top_freqs[i]
             if await_count <= 0:
                 thresholds.append(i+1)
                 break
-        if thresholds[-1] == 1:
-            print(top_freqs[:20])
-        # print(thresholds[-1])
     return thresholds
 
 if __name__ == '__main__':
-    width, swaps_per_round = int(sys.argv[1]), int(sys.argv[2])
-    rounds = width * 4
-    thresholds = test(width, rounds, swaps_per_round)
-    for i in range(0, rounds, 10):
+    args = {}
+    for x in sys.argv:
+        if '=' in x:
+            prefix, postfix = x[:x.index('=')], x[x.index('=')+1:]
+            args[prefix] = int(postfix)
+        for arg, val in DEFAULTS.items():
+            if arg not in args:
+                print("Arg {} defaulted to {}".format(arg, val))
+                args[arg] = val
+    LOG_LEVEL[0] = args['log_level']
+    thresholds = test(args['width'], args['rounds'], args['swaps_per_round'])
+    for i in range(0, args['rounds'], 10):
         print("After {} rounds, need to DoS {} validators for 20% chance of killing proposer".format(i, thresholds[i]))
     second_half = thresholds[len(thresholds)//2:]
-    print("Average of second half: {}".format(sum(second_half) / len(second_half)))
-    print("Frequency of 1 in second half: {}".format(second_half.count(1) / len(second_half)))
+    print("Average 20%-diffusion of proposer: {}".format(sum(second_half) / len(second_half)))
+    print("Frequency of 20%-diffusion = 1: {}".format(second_half.count(1) / len(second_half)))
