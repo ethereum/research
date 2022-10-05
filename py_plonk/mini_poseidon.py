a26cd10596a845040322576acbeb621c5040b483
vub <vub@pop-os.localdomain>
2022-10-05 06:59:54 -0500

    Cleaned up mini poseidon

--- py_plonk/mini_poseidon.py
+++ py_plonk/mini_poseidon.py
@@ -5,41 +5,18 @@ import json
 class f_inner(Field):
     field_modulus = b.curve_order
 
-#rc = [
-#    [f_inner(a), f_inner(b)] for (a,b) in json.load(open('rc.json'))
-#]
-
-#mds_matrix = [
-#    [f_inner(1) / 2, f_inner(1) / 3],
-#    [f_inner(1) / 3, f_inner(1) / 4]
-#]
-
 # Mimics the Poseidon hash for params:
 #
 # p                    = b.curve_order
 # security level       = 128
 # alpha                = 5
 # input size           = 2
-# t (inner state size) = 2
+# t (inner state size) = 3
 # full round count     = 8 (4 on each side)
 # partial round count  = 56
 #
 # Tested compatible with the implementation at
 # https://github.com/ingonyama-zk/poseidon-hash
-
-def gash(L, R):
-    L, R = f_inner(L), f_inner(R)
-    for i in range(64):
-        L = (L + rc[i][0]) ** 5
-        R += rc[i][1]
-        if i < 4 or i >= 60:
-            R = R ** 5
-
-        (L, R) = (
-            (L * mds_matrix[0][0] + R * mds_matrix[1][0]),
-            (L * mds_matrix[0][1] + R * mds_matrix[1][1]),
-        )
-    return R
 
 rc = [
     [f_inner(a), f_inner(b), f_inner(c)]
@@ -58,7 +35,6 @@ def hash(in1, in2):
             M = M ** 5
             R = R ** 5
 
-        # print('mid', i, L, M, R)
         (L, M, R) = (
             (L * mds[0] + M * mds[1] + R * mds[2]),
             (L * mds[1] + M * mds[2] + R * mds[3]),
