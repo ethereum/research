b976e82d91c1edd4fe8d28098e734d0715a87134
vub <Vitalik Buterin>
2024-03-25 22:13:32 +0800

    Added new analysis code

--- correlation_analysis/analyze_attestations.py
+++ correlation_analysis/analyze_attestations.py
@@ -3,9 +3,11 @@ import os
 import requests
 
 ATTESTER_FILE = 'attesters.txt'
+IMMEDIATE_ATTESTER_FILE = 'immediate_attesters.txt'
 COMMITTEES_FILE = 'committees.txt'
 CLUSTERS_FILE = 'clusters.csv'
 FILES_URL = 'https://data.ethpandaops.io/efresearch/'
+MAX_VALIDATOR_ID = 1400000
 
 import bisect
 import struct
@@ -55,46 +57,33 @@ def json_cached_execute(fun, filename):
         return o
 
 # Output mapping {slot: set(validators who attested in that slot)}
-def get_attesters_by_slot():
-    attesters_by_slot = {}
-    if ATTESTER_FILE not in os.listdir():
+def cached_load_from_web_db_file(filename):
+    if filename not in os.listdir():
         print("Loading attesters file from internet")
-        contents = requests.get(FILES_URL + ATTESTER_FILE).text
-        open(ATTESTER_FILE, 'w').write(contents)
-    print("Processing attesters file")
-    for i, line in enumerate(open(ATTESTER_FILE).readlines()):
-        if line:
-            slot, attesters = line.split('\t')
-            attesters_by_slot[int(slot)] = fast_set({
-                int(x) for x in attesters.strip()[1:-1].split(',')
-            })
-        if i % 100 == 0:
-            print("Processed {} lines".format(i))
-    return attesters_by_slot
+        contents = requests.get(FILES_URL + filename).text
+        open(filename, 'w').write(contents)
+    if filename+'.pickle' not in os.listdir():
+        print("Processing attesters file")
+        obj = {}
+        for i, line in enumerate(open(filename).readlines()):
+            if line:
+                slot, attesters = line.split('\t')
+                obj[int(slot)] = fast_set({
+                    int(x) for x in attesters.strip()[1:-1].split(',')
+                })
+            if i % 100 == 0:
+                print("Processed {} lines".format(i))
+        pickle.dump(obj, open(filename+'.pickle', 'wb'))
+        return obj
+    else:
+        return pickle.load(open(filename+'.pickle', 'rb'))
 
-attesters_by_slot = json_cached_execute(get_attesters_by_slot, 'attesters_by_slot.pickle')
+immediate_attesters_by_slot = cached_load_from_web_db_file('immediate_attesters.txt')
+print("Processed immediate attesters")
+attesters_by_slot = cached_load_from_web_db_file('attesters.txt')
 print('Processed attesters')
-
-# Output mapping {slot: set(validators who should have attested in that slot)}
-def get_committees_by_slot():
-    committees_by_slot = {}
-    if COMMITTEES_FILE not in os.listdir():
-        print("Loading committees file from internet")
-        contents = requests.get(FILES_URL + COMMITTEES_FILE).text
-        open(COMMITTEES_FILE, 'w').write(contents)
-    print("Processing committees file")
-    for i, line in enumerate(open(COMMITTEES_FILE).readlines()):
-        if line:
-            slot, members = line.split('\t')
-            committees_by_slot[int(slot)] = fast_set({
-                int(x) for x in members.strip()[1:-1].split(',')
-            })
-        if i % 100 == 0:
-            print("Processed {} lines".format(i))
-    return committees_by_slot
-
-committees_by_slot = json_cached_execute(get_committees_by_slot, 'committees_by_slot.pickle')
-print("Processed committees")
+committees_by_slot = cached_load_from_web_db_file('committees.txt')
+print('Processed committees')
 
 def get_all_validators():
     all_validators = set()
@@ -104,7 +93,6 @@ def get_all_validators():
     return all_validators
 
 all_validators = json_cached_execute(get_all_validators, 'all_validators.pickle')
-
 print("Found all validators")
 
 # Output mapping {attester: their cluster} and {cluster: set(attesters in cluster)}
@@ -152,25 +140,26 @@ max_slot = max(committees_by_slot.keys())
 historical_fumbles = [0] * 32
 # Definition of a miss: missed the current and previous epochs
 historical_misses = [0] * 32
+# Like a fumble or miss but for the single-slot set
+historical_ssfumbles = [0] * 32
+historical_ssmisses = [0] * 32
 
-# Track totals
-hits, fumbles, misses = 0, 0, 0
+# For (fumble, miss, ssfumble, ssmiss): instances of two distinct
+# validators in the same cluster making this error within the same
+# slot
+co_totals = [0, 0, 0, 0]
+expected_co_totals = [0, 0, 0, 0]
 
-# "Co-fumbler" = pair of distinct validators in the same cluster that
-# fumbles in the same epoch
-co_fumblers = 0
-expected_co_fumblers = 0
-# "Co-miss" = pair of distinct validators in the same cluster that
-# miss in the same epoch
-co_missers = 0
-expected_co_missers = 0
-
-# For each of four penalty schemes, store the total penalties each
+# For each of penalty schemes, store the total penalties each
 # validator receives according to that scheme
-total_penalties_basic = {v: 0 for v in all_validators}
-total_penalties_fumbles_only = {v: 0 for v in all_validators}
-total_penalties_anticorr = {v: 0 for v in all_validators}
-total_penalties_excess = {v: 0 for v in all_validators}
+schemes = ('basic', 'fumbles_only', 'ss_only', 'ss_fumblers_only', 'excess', 'ss_excess')
+penalties = {
+    scheme: {v: 0 for v in all_validators}
+    for scheme in schemes
+}
+
+cluster_failures = {c: [0,0,0,0] for c in cluster_to_attesters}
+processed_slots = 0
 
 # Get the previous slot that `validator_id` was supposed to attest in.
 # May return None if eg. the given slot is the validator's first active slot.
@@ -179,63 +168,117 @@ def get_prev_expected_slot(validator_id, slot):
         if validator_id in committees_by_slot[s]:
             return s
 
+for slot in range(min_slot, max_slot):
+    # Skip the first two epochs to make sure that there is a previous slot
+    if slot not in committees_by_slot or slot - 63 not in committees_by_slot:
+        continue
+    # Skip slots where the next slot is empty
+    if slot not in immediate_attesters_by_slot:
+        continue
+
 # Compute the statistics across our full range of slots
 for slot in range(min_slot, max_slot):
     # Skip the first two epochs to make sure that there is a previous slot
     if slot not in committees_by_slot or slot - 63 not in committees_by_slot:
         continue
+    # Skip slots where the next slot is empty
+    if slot not in immediate_attesters_by_slot:
+        continue
     this_slot_attesters = frozenset(x for x in attesters_by_slot[slot])
+    this_slot_immediate_attesters = frozenset(x for x in immediate_attesters_by_slot[slot])
+    # Definitions:
+    # Fumble: missed in this epoch, attested in previous epoch
+    # Miss: missed in this epoch and previous epoch
+    # SSfumble (single-slot fumble): fumbles, but for the dataset of
+    # attestations included within one slot
+    # SSmiss: you get it
     fumblers = set()
     missers = set()
-    committee_size = len(committees_by_slot[slot])
+    ssfumblers = set()
+    ssmissers = set()
+    committee = committees_by_slot[slot]
+    committee_size = len(committee)
     # Sometimes the DB we're using screws up and returns a 2x too big committee
     # Skip those slots
     if committee_size > len(all_validators) / 24:
         print(f"Skipping slot {slot} (known miscalculated committee)")
+        del committees_by_slot[slot]
         continue
     # Main loop: add fumbles and misses
-    for validator in committees_by_slot[slot]:
-        if validator in this_slot_attesters:
-            hits += 1
-        elif validator in attesters_by_slot.get(get_prev_expected_slot(validator, slot), {}):
-            fumbles += 1
-            fumblers.add(validator)
-        else:
-            misses += 1
-            missers.add(validator)
-    relative_fumbler_rate = len(fumblers) / ((sum(historical_fumbles[-32:]) + 1) / 32)
-    relative_misser_rate = len(missers) / ((sum(historical_misses[-32:]) + 1) / 32)
+    for validator in committee:
+        cluster = attester_to_cluster[validator]
+        if validator not in this_slot_immediate_attesters:
+            prev_slot = get_prev_expected_slot(validator, slot)
+            if validator in immediate_attesters_by_slot.get(prev_slot, {}):
+                ssfumblers.add(validator)
+                cluster_failures[cluster][2] += 1
+            else:
+                ssmissers.add(validator)
+                cluster_failures[cluster][3] += 1
+            if validator not in this_slot_attesters:
+                if validator in attesters_by_slot.get(prev_slot, {}):
+                    fumblers.add(validator)
+                    cluster_failures[cluster][0] += 1
+                else:
+                    missers.add(validator)
+                    cluster_failures[cluster][1] += 1
+    # Track co-fumbler, co-misser, etc stats
+    for i, _set in enumerate((fumblers, missers, ssfumblers, ssmissers)):
+        for member in _set:
+            cluster_mates = cluster_to_attesters[attester_to_cluster[member]]
+            co_totals[i] += len(_set.intersection(cluster_mates)) - 1
+            expected_co_totals[i] += (
+                (len(cluster_mates) - 1) *
+                (len(_set) - 1) / (len(all_validators) - 1)
+            )
+    relative_failure_rate = (
+        (len(fumblers) + len(missers)) /
+        ((sum(historical_fumbles[-32:] + historical_misses[-32:]) + 1) / 32)
+    )
+    relative_ssfailure_rate = (
+        (len(ssfumblers) + len(ssmissers)) /
+        ((sum(historical_ssfumbles[-32:] + historical_ssmisses[-32:]) + 1) / 32)
+    )
+    # Now, apply the penalty schemes
+    # schemes = ('basic', 'fumbles_only', 'ss_only', 'ss_fumbles_only', 'excess', 'ss_excess')
     for fumbler in fumblers:
-        total_penalties_basic[fumbler] += 1
-        total_penalties_fumbles_only[fumbler] += 1
-        total_penalties_anticorr[fumbler] += (len(fumblers) / (committee_size)) ** 0.2
-        total_penalties_excess[fumbler] += max(0, min(relative_fumbler_rate - 1, 5))
-        cluster_mates = cluster_to_attesters[attester_to_cluster[fumbler]]
-        co_fumblers += len(fumblers.intersection(cluster_mates)) - 1
-        # Expected co-fumblers = co-fumblers (pairs of fumblers in the same cluster)
-        # if fumbling was truly uncorrelated
-        expected_co_fumblers += (
-            (len(cluster_mates) - 1) *
-            (len(fumblers) - 1) / (len(all_validators) - 1)
-        )
+        penalties['basic'][fumbler] += 1
+        penalties['fumbles_only'][fumbler] += 1
+        penalties['excess'][fumbler] += max(0, min(relative_failure_rate - 1, 4))
     for misser in missers:
-        total_penalties_basic[fumbler] += 1
-        total_penalties_anticorr[fumbler] += (len(fumblers) / (committee_size)) ** 0.2
-        total_penalties_excess[fumbler] += max(0, min(relative_fumbler_rate - 1, 5))
-        cluster_mates = cluster_to_attesters[attester_to_cluster[misser]]
-        co_missers += len(missers.intersection(cluster_mates)) - 1
-        expected_co_missers += (
-            (len(cluster_mates) - 1) *
-            (len(missers) - 1) / (len(all_validators) - 1)
-        )
+        penalties['basic'][misser] += 1
+        penalties['excess'][misser] += max(0, min(relative_failure_rate - 1, 4))
+    for ssfumbler in ssfumblers:
+        penalties['ss_only'][ssfumbler] += 1
+        penalties['ss_fumblers_only'][ssfumbler] += 1
+        penalties['ss_excess'][ssfumbler] += max(0, min(relative_ssfailure_rate - 1, 4))
+    for ssmisser in ssmissers:
+        penalties['ss_only'][ssmisser] += 1
+        penalties['ss_excess'][ssmisser] += max(0, min(relative_ssfailure_rate - 1, 4))
     historical_fumbles.append(len(fumblers))
     historical_misses.append(len(missers))
-    print("Up to slot {}, {} fumbles and {} misses in this slot".format(
-        slot, len(fumblers), len(missers)
+    historical_ssfumbles.append(len(ssfumblers))
+    historical_ssmisses.append(len(ssmissers))
+    print("\nUp to slot {} ({} in committee): {} fumbles, {} misses, {} ssfumbles, {} ssmisses".format(
+        slot, len(committee), len(fumblers), len(missers), len(ssfumblers), len(ssmissers)
     ))
     print("Cumulative stats: {} co-fumblers ({:.2f} expected), {} co-missers, ({:.2f} expected)".format(
-        co_fumblers, expected_co_fumblers, co_missers, expected_co_missers
+        co_totals[0], expected_co_totals[0], co_totals[1], expected_co_totals[1]
     ))
+    print("Single slot: {} co-fumblers ({:.2f} expected), {} co-missers, ({:.2f} expected)".format(
+        co_totals[2], expected_co_totals[2], co_totals[3], expected_co_totals[3]
+    ))
+    processed_slots += 1
+
+# Compute per-cluster failure rates
+
+expected_co_totals2 = [0, 0, 0, 0]
+for cluster, attesters in cluster_to_attesters.items():
+    for i in range(4):
+        failure_rate = cluster_failures[cluster][i] / (processed_slots / 32) / len(attesters)
+        expected_co_totals2[i] += ((len(attesters) / 32) * failure_rate) ** 2 * processed_slots
+print("Expectations: {:.2f} co-fumblers, {:.2f} co-missers, {:.2f} co-ssfumblers, {:.2f} co-ssmissers".format(*expected_co_totals2))
+
 
 # Take a random selection of clusters at different sizes, chance of being
 # selected is proportional to size, up to 100% for >= 10000
@@ -257,28 +300,45 @@ chosen_clusters['Combined labeled'] = set.union(*chosen_clusters.values())
 extra_clusters['All'] = all_validators
 
 print("\nShowing simulated penalties for different clusters, using different penalty rules, normalized:\n")
+def average_for_set(source, validators):
+    return sum(source[m] for m in validators) / len(validators)
+
+global_avg_penalties = {
+    scheme: average_for_set(penalties[scheme], all_validators)
+    for scheme in schemes
+}
+
 shown_clusters = (
     sorted(extra_clusters.items(), key=lambda x: -len(x[1])) +
     sorted(chosen_clusters.items(), key=lambda x: -len(x[1]))
 )
-def average_for_set(source, validators):
-    return sum(source[m] for m in validators) / len(validators)
 
-global_avg_penalties_basic = average_for_set(total_penalties_basic, all_validators)
-global_avg_penalties_fumbles_only = average_for_set(total_penalties_fumbles_only, all_validators)
-global_avg_penalties_anticorr = average_for_set(total_penalties_anticorr, all_validators)
-global_avg_penalties_excess = average_for_set(total_penalties_excess, all_validators)
+first_column_width = max(len(x[0]) for x in shown_clusters) + 2
+
+def fillto(text, k):
+    return text + ' ' * (k - len(text))
+
+first_line = fillto('', first_column_width)
+for scheme in schemes:
+    first_line += fillto(scheme, len(scheme) + 2)
+print(first_line)
+
 for name, members in shown_clusters:
-    print("{}: basic {:.3f}, fumbles only {:.3f}, anti-correlation {:.3f} excess {:.3f}".format(
-        name,
-        average_for_set(total_penalties_basic, members) / global_avg_penalties_basic,
-        average_for_set(total_penalties_fumbles_only, members) / global_avg_penalties_fumbles_only,
-        average_for_set(total_penalties_anticorr, members) / global_avg_penalties_anticorr,
-        average_for_set(total_penalties_excess, members) / global_avg_penalties_excess,
-    ))
+    line = fillto(name, first_column_width)
+    for scheme in schemes:
+        normalized_value = average_for_set(penalties[scheme], members) / global_avg_penalties[scheme]
+        line += fillto('{:.3f}'.format(normalized_value), len(scheme) + 2)
+    print(line)
 
-print("\nTotal stats: {} hits ({:.2f}%), {} fumbles ({:.2f}%), {} misses ({:.2f}%)".format(
-    hits, hits * 100 / (hits + fumbles + misses),
-    fumbles, fumbles * 100 / (hits + fumbles + misses),
-    misses, misses * 100 / (hits + fumbles + misses),
+fumbles = sum(historical_fumbles)
+misses = sum(historical_misses)
+ssfumbles = sum(historical_ssfumbles)
+ssmisses = sum(historical_ssmisses)
+total = sum(len(x) for x in committees_by_slot.values())
+
+print("\nTotal stats: {} fumbles ({:.2f}%), {} misses ({:.2f}%), {} ssfumbles ({:.2f}%), {} ssmisses ({:.2f}%)".format(
+    fumbles, fumbles * 100 / total,
+    misses, misses * 100 / total,
+    ssfumbles, ssfumbles * 100 / total,
+    ssmisses, ssmisses * 100 / total,
 ))
