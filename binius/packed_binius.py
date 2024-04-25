05a5a2c82ba9b8adb07e38397a3751d4b5528ec9
vub <Vitalik Buterin>
2024-04-25 19:31:11 +0200

    Packed binius works again

--- binius/packed_binius.py
+++ binius/packed_binius.py
@@ -3,8 +3,63 @@ NUM_CHALLENGES = 4
 PACKING_FACTOR = 32
 
 from binary_fields import BinaryFieldElement
-from utils import get_class, eval_poly_at, mul_polys, compute_lagrange_poly, multilinear_poly_eval, extend, evaluation_tensor_product, Vector, zero_of_same_type
+from utils import get_class, eval_poly_at, mul_polys, compute_lagrange_poly, multilinear_poly_eval, extend, evaluation_tensor_product
 from merkle import hash, merkelize, get_root, get_branch, verify_branch
+
+# Suppose that we will be taking in `items` bits, and treating those bits as
+# items // PACKING_FACTOR field elements. Now, imagine we Reed-Solomon extend
+# those field elements. The index-coord bit of the extended value (ie. the
+# index % PACKING_FACTOR bit of the index // PACKING_FACTOR field element) is
+# a linear function of the input bits. Specifically, it must be a sum of a
+# particular subset of the input bits.
+#
+# Which subset? The answer is independent of the input. We compute what that
+# subset is, and return a vector that is `items` bits long, where the index-k
+# bit represents whether or not bit k is part of that subset.
+
+def generate_reed_solomon_matrix_row(items, coord):
+    poly_width = items // PACKING_FACTOR
+    evals = [
+        eval_poly_at(compute_lagrange_poly(poly_width, BinaryFieldElement(i)), coord // PACKING_FACTOR)
+        for i in range(poly_width)
+    ]
+    o = [None for _ in range(items)]
+    for i in range(poly_width):
+        value = evals[i]
+        for bit in range(PACKING_FACTOR):
+            o[i * PACKING_FACTOR + bit] = ((value * (1<<bit)).value >> (coord % PACKING_FACTOR)) & 1
+    return o
+
+# Generates the full matrix representing the operation of extending `items`
+# bits to `items * expansion_factor` bits, by treating the input bits as
+# packed field elements and Reed-Solomon extending them
+def generate_reed_solomon_matrix(items, expansion_factor=2):
+    return [
+        generate_reed_solomon_matrix_row(items, coord)
+        for coord in range(items, items * expansion_factor)
+    ]
+
+# Use the matrix row to actually compute a particular bit of the extension
+def apply_reed_solomon_matrix_row(vals, row):
+    return sum(
+        [v for v,bit in zip(vals, row) if bit == 1],
+        BinaryFieldElement(0)
+    )
+
+# Actually compute the extension
+def extend_via_matrix(vals, matrix):
+    return vals + [
+        apply_reed_solomon_matrix_row(vals, row)
+        for row in matrix
+    ]
+
+# Pack `PACKING_FACTOR` items into a field element
+def pack(item):
+    assert len(item) == PACKING_FACTOR
+    o = BinaryFieldElement(0)
+    for i, v in enumerate(item):
+        o += v * (2**i)
+    return o
 
 # "Block-level encoding-based polynomial commitment scheme", section 3.11 of
 # https://eprint.iacr.org/2023/1784.pdf
@@ -22,33 +77,37 @@ def packed_binius_proof(evaluations, evaluation_point):
     row_count = 1 << ((L + 1) // 2)
     rows = [evaluations[i:i+row_length] for i in range(0, len(evaluations), row_length)]
 
-    # Difference with 3.7: here, we group the bits within a row into blocks of size
-    # PACKING_FACTOR each
-    packed_rows = [
-        [Vector(row[i:i+PACKING_FACTOR]) for i in range(0, len(row), PACKING_FACTOR)]
-        for row in rows
-    ]
-    packed_row_length = row_length // PACKING_FACTOR
+    # Extend each row using a Reed-Solomon code
+    # Difference with 3.7: here, we use a bitwise approach to extend the rows
+    M = generate_reed_solomon_matrix(row_length, EXPANSION_FACTOR)
+    extended_rows = [extend_via_matrix(row, M) for row in rows]
+    extended_row_length = row_length * EXPANSION_FACTOR
 
-    # We extend the packed groups of bits
-    extended_rows = [extend(row, expansion_factor=EXPANSION_FACTOR) for row in packed_rows]
-    extended_row_length = packed_row_length * EXPANSION_FACTOR
+    # Compute t_prime, a linear combination of the rows
     row_combination = evaluation_tensor_product(evaluation_point[L//2:])
-
     assert len(row_combination) == len(rows) == row_count
     t_prime = [
         sum([rows[i][j] * row_combination[i] for i in range(row_count)], BinaryFieldElement(0))
         for j in range(row_length)
     ]
-    columns = [[row[j] for row in extended_rows] for j in range(extended_row_length)]
-    bytes_per_element = 1
-    while 256**bytes_per_element < row_length:
-        bytes_per_element *= 2
-    packed_columns = [b''.join(x.to_bytes(bytes_per_element, 'little') for x in col) for col in columns]
+
+    # Pack columns into a Merkle tree, to commit to them
+    columns = [
+        [row[j:j+PACKING_FACTOR] for row in extended_rows]
+        for j in range(0, extended_row_length, PACKING_FACTOR)
+    ]
+    bytes_per_element = PACKING_FACTOR//8
+    packed_columns = [
+        b''.join(pack(x).to_bytes(bytes_per_element, 'little') for x in col)
+        for col in columns
+    ]
     merkle_tree = merkelize(packed_columns)
     root = get_root(merkle_tree)
+
+    # Challenge in a few positions, to get branches
+    packed_length = extended_row_length // PACKING_FACTOR
     challenges = [
-        int.from_bytes(hash(root + i.to_bytes(4, 'little')), 'little') % extended_row_length
+        int.from_bytes(hash(root + bytes([i])), 'little') % packed_length
         for i in range(NUM_CHALLENGES)
     ]
     return {
@@ -61,37 +120,64 @@ def packed_binius_proof(evaluations, evaluation_point):
     }
 
 def verify_packed_binius_proof(proof):
-    # Check Merkle branches
-    root = proof["root"]
-    extended_row_length = 2**len(proof["branches"][0])
+    columns, evaluation_point, value, t_prime, root, branches = (
+        proof['columns'],
+        proof['evaluation_point'],
+        proof['eval'],
+        proof['t_prime'],
+        proof['root'],
+        proof['branches'],
+    )
+
+    # Compute the row length and row count of the grid. Should output same
+    # numbers as what prover gave
+    L = len(evaluation_point)
+    row_length = 1 << (L // 2)
+    row_count = 1 << ((L + 1) // 2)
+    extended_row_length = row_length * EXPANSION_FACTOR
+
+    # Compute challenges. Should output the same as what prover computed
+    packed_length = extended_row_length // PACKING_FACTOR
     challenges = [
-        int.from_bytes(hash(root + i.to_bytes(4, 'little')), 'little') % extended_row_length
+        int.from_bytes(hash(root + bytes([i])), 'little') % packed_length
         for i in range(NUM_CHALLENGES)
     ]
-    packed_row_length = extended_row_length // EXPANSION_FACTOR
-    row_length = packed_row_length * PACKING_FACTOR
-    row_count = len(proof['columns'][0])
-    bytes_per_element = 1
-    while 256**bytes_per_element < packed_row_length:
-        bytes_per_element *= 2
-    for challenge, branch, column in zip(challenges, proof['branches'], proof['columns']):
-        packed_column = b''.join(x.to_bytes(bytes_per_element, 'little') for x in column)
+
+    # Verify the correctness of the Merkle branches
+    bytes_per_element = PACKING_FACTOR//8
+    for challenge, branch, col in zip(challenges, branches, columns):
+        packed_column = \
+            b''.join(pack(x).to_bytes(bytes_per_element, 'little') for x in col)
         print(f"Verifying Merkle branch for column {challenge}")
         assert verify_branch(root, challenge, packed_column, branch)
-    t_prime = proof["t_prime"]
-    packed_t_prime = [Vector(t_prime[i:i+PACKING_FACTOR]) for i in range(0, len(t_prime), PACKING_FACTOR)]
-    extended_t_prime = extend(packed_t_prime, expansion_factor=EXPANSION_FACTOR)
-    log_col_count = row_length.bit_length() - 1
-    print('log col count', log_col_count)
-    row_combination = evaluation_tensor_product(proof["evaluation_point"][log_col_count:])
+
+    # Use the same Reed-Solomon code that the prover used to extend the rows,
+    # but to extend t_prime
+    M = generate_reed_solomon_matrix(row_length, EXPANSION_FACTOR)
+    extended_t_prime = extend_via_matrix(t_prime, M)
+
+    # Here, we take advantage of the linearity of the code. A linear combination
+    # of the Reed-Solomon extension gives the same result as an extension of the
+    # linear combination.
+    row_combination = evaluation_tensor_product(proof["evaluation_point"][L // 2:])
     for column, challenge in zip(proof['columns'], challenges):
-        print(len(column), len(row_combination), row_count)
-        expected_tprime = sum([column[i] * row_combination[i] for i in range(row_count)], zero_of_same_type(column[0]))
-        print(f"Testing challenge on column {challenge}: expected {expected_tprime} computed {extended_t_prime[challenge]}")
-        assert expected_tprime == extended_t_prime[challenge]
-    col_combination = evaluation_tensor_product(proof["evaluation_point"][:log_col_count])
-    print('col comb', col_combination)
-    computed_eval = sum([proof["t_prime"][i] * col_combination[i] for i in range(row_length)], BinaryFieldElement(0))
-    print(f"Testing evaluation: expected {proof['eval']} computed {computed_eval}")
-    assert computed_eval == proof["eval"]
+        for b in range(PACKING_FACTOR):
+            computed_tprime = sum(
+                [column[i][b] * row_combination[i] for i in range(row_count)],
+                BinaryFieldElement(0)
+            )
+            expected_tprime = extended_t_prime[challenge * PACKING_FACTOR + b]
+            assert expected_tprime == computed_tprime
+    print("T_prime matches Merkle branches")
+
+    # Take the right linear combination of elements *within* t_prime to
+    # extract the evaluation of the original multilinear polynomial at
+    # the desired point
+    col_combination = evaluation_tensor_product(evaluation_point[:L // 2])
+    computed_eval = sum(
+        [t_prime[i] * col_combination[i] for i in range(row_length)],
+        BinaryFieldElement(0)
+    )
+    print(f"Testing evaluation: expected {value} computed {computed_eval}")
+    assert computed_eval == value
     return True
--- binius/test.py
+++ binius/test.py
@@ -1,6 +1,16 @@
 from binary_fields import BinaryFieldElement as B
+from utils import extend
 from simple_binius import simple_binius_proof, verify_simple_binius_proof
-from packed_binius import packed_binius_proof, verify_packed_binius_proof
+from packed_binius import (
+    packed_binius_proof, verify_packed_binius_proof,
+    generate_reed_solomon_matrix, extend_via_matrix, pack
+)
+
+def test_binary_operations():
+    assert (B(3) + B(14)) * B(15) == B(3) * B(15) + B(14) * B(15)
+    assert len(set((B(x) * 3).value for x in range(256))) == 256
+    assert B(13) ** 255 == 1
+    print("Verified basic operations")
 
 def test_simple_binius():
     z = [B(int(bit)) for bit in bin(3**7890)[2:][:1024]]
@@ -9,6 +19,21 @@ def test_simple_binius():
     print("t_prime:", proof["t_prime"])
     verify_simple_binius_proof(proof)
     print("Verified simple-binius proof")
+
+def test_matrix_rs_encoding():
+    z = [B(int(bit)) for bit in bin(3**7890)[2:][:128]]
+    M = generate_reed_solomon_matrix(128, 2)
+    z_extended = extend_via_matrix(z, M)
+
+
+
+    z_packed = [pack(z[i:i+32]) for i in range(0, len(z), 32)]
+    z_packed_extended = extend(z_packed)
+    print('z_packed_extended', z_packed_extended)
+    z_extended_packed = [pack(z_extended[i:i+32]) for i in range(0, len(z_extended), 32)]
+    print('z_extended_packed', z_extended_packed)
+    assert z_packed_extended == z_extended_packed
+    print("Verified matrix Reed-Solomon encoding")
 
 def test_packed_binius():
     z = [B(int(bit)) for bit in bin(3**7890)[2:][:1024]]
@@ -19,5 +44,7 @@ def test_packed_binius():
     print("Verified packed-binius proof")
 
 if __name__ == '__main__':
+    test_binary_operations()
     test_simple_binius()
+    test_matrix_rs_encoding()
     test_packed_binius()
