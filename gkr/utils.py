88950c89893a2f8be29e8587b0b67fbd90b2c815
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-09-13 13:29:24 +0300

    fix: condition checks for `arg.ndim` ordering

--- gkr/utils.py
+++ gkr/utils.py
@@ -4,18 +4,23 @@ def hash(*args):
     o = koalabear.KoalaBear(koalabear.koalabear_field.arange(8))
     for i, arg in enumerate(args):
         o += koalabear.KoalaBear.sum(o, axis=0)
-        if arg.ndim == 0 and isinstance(arg, koalabear.KoalaBear):
+
+        if isinstance(arg, koalabear.KoalaBear) and arg.ndim == 0:
             o[1] += arg
             o = (o + i) ** 5
+
         elif isinstance(arg, koalabear.KoalaBear):
             for j in range(0, arg.shape[0], 8):
-                width = min(8, arg.shape[0]-j)
+                width = min(8, arg.shape[0] - j)
                 o[:width] += arg[j:j+width]
                 o = (o + i) ** 5
-        elif arg.ndim == 0 and isinstance(arg, koalabear.ExtendedKoalaBear):
+
+        elif isinstance(arg, koalabear.ExtendedKoalaBear) and arg.ndim == 0:
             o[:4] += koalabear.KoalaBear(arg.value)
             o = (o + i) ** 5
+
         else:
             raise Exception("not implemented")
+
     o += koalabear.KoalaBear.sum(o, axis=0)
     return o
